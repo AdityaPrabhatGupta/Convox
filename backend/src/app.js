515f2fb6524431d547808b517a256db091770807
ADITYA PRABHAT GUPTA <aditygupta2474@gmail.com>
2026-05-04 14:49:49 +0530

    add health route

--- backend/src/app.js
+++ backend/src/app.js
@@ -37,6 +37,10 @@ app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
 //Health Check
+app.get("/health", (req, res) => {
+    res.status(200).send("OK");
+});
+
 app.get("/api/health", (req, res) => {
     res.status(200).json({
         status: "ok",
